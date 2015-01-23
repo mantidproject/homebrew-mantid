0e37168b75e283ca0eccfe8661ff1bcb08ab7eff
Stuart Campbell <stuart.i.campbell@gmail.com>
2015-01-22 21:41:49 -0500

    Remove comments that were there for testing.
    I commented out a number of the dependencies to help with speed of testing.  Probably best to add them back in!

--- mantid-developer.rb
+++ mantid-developer.rb
@@ -7,25 +7,25 @@ class MantidDeveloper < Formula
 	homepage "https://github.com/mantidproject/mantid/wiki/Building-Mantid-on-OS-X-10.9-&-10.10-using-clang-and-Xcode"
 	version '1.1'
 
-#	depends_on "qt" => "with-qt3support"
+	depends_on "qt" => "with-qt3support"
 	depends_on "cmake"
-#	depends_on "sip" => "build-from-source"
-#	depends_on "pyqt" => "build-from-source"
-#	depends_on "qscintilla2" => "build-from-source"
-#	depends_on "poco" => "c++11"
-#	depends_on "boost" => "c++11"
-#	depends_on "boost-python" => "c++11"
-#	depends_on "gsl"
-#	depends_on "hdf5" => ["with-cxx", "c++11", "with-fortran", "with-fortran2003", "without-check"]
-#	depends_on "libmxml"
+	depends_on "sip" => "build-from-source"
+	depends_on "pyqt" => "build-from-source"
+	depends_on "qscintilla2" => "build-from-source"
+	depends_on "poco" => "c++11"
+	depends_on "boost" => "c++11"
+	depends_on "boost-python" => "c++11"
+	depends_on "gsl"
+	depends_on "hdf5" => ["with-cxx", "c++11", "with-fortran", "with-fortran2003", "without-check"]
+	depends_on "libmxml"
 	depends_on "muparser"
-#	depends_on "nexusformat" => "c++11"
+	depends_on "nexusformat" => "c++11"
 	depends_on "jsoncpp"
-#	depends_on "oce" => "without-tbb"
-#	depends_on "openssl"
-#	depends_on "qwt5"
-#	depends_on "qwtplot3d"
-#	depends_on "google-perftools"
+	depends_on "oce" => "without-tbb"
+	depends_on "openssl"
+	depends_on "qwt5"
+	depends_on "qwtplot3d"
+	depends_on "google-perftools"
 
 	# Now let's see if we can install some python stuff
 	# Not sure how best to check if these are already installed! 
