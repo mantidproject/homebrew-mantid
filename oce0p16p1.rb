1d3dc47880b518034e8f8799da17e7126e126bcc
Steven Hahn <hahnse@ornl.gov>
2015-04-30 17:36:03 -0400

    Create oce0p16p1.rb

---
+++ oce0p16p1.rb
@@ -0,0 +1,46 @@
+require "formula"
+
+class Oce < Formula
+  homepage "https://github.com/tpaviot/oce"
+  url "https://github.com/tpaviot/oce/archive/OCE-0.16.1.tar.gz"
+  sha256 "d31030c8da4a1b33f767d0d59895a995c8eabc8fc65cbe0558734f6021ea2f57"
+
+  conflicts_with "opencascade", :because => "OCE is a fork for patches/improvements/experiments over OpenCascade"
+
+  option "without-opencl", "Build without OpenCL support"
+
+  depends_on "cmake" => :build
+  depends_on "freetype"
+  depends_on "ftgl"
+  depends_on "freeimage" => :recommended
+  depends_on "gl2ps" => :recommended
+  depends_on "tbb" => :recommended
+  depends_on :macos => :snow_leopard
+
+  def install
+    cmake_args = std_cmake_args
+    cmake_args << "-DOCE_INSTALL_PREFIX:STRING=#{prefix}"
+    cmake_args << "-DOCE_COPY_HEADERS_BUILD:BOOL=ON"
+    cmake_args << "-DOCE_DRAW:BOOL=ON"
+    cmake_args << "-DOCE_MULTITHREAD_LIBRARY:STRING=TBB" if build.with? "tbb"
+    cmake_args << "-DFREETYPE_INCLUDE_DIRS=#{Formula["freetype"].opt_include}/freetype2"
+
+    %w{freeimage gl2ps}.each do |feature|
+      cmake_args << "-DOCE_WITH_#{feature.upcase}:BOOL=ON" if build.with? feature
+    end
+
+    opencl_path = Pathname.new "/System/Library/Frameworks/OpenCL.framework"
+    if build.with?("opencl") && opencl_path.exist?
+      cmake_args << "-DOCE_WITH_OPENCL:BOOL=ON"
+      cmake_args << "-DOPENCL_LIBRARIES:PATH=#{opencl_path}"
+      cmake_args << "-D_OPENCL_CPP_INCLUDE_DIRS:PATH=#{opencl_path}/Headers"
+    end
+
+    system "cmake", ".", *cmake_args
+    system "make", "install/strip"
+  end
+
+  test do
+    "1\n"==`#{bin}/DRAWEXE -c \"pload ALL\"`
+  end
+end
