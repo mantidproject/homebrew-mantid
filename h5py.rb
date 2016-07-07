f9fa2f909660d850e8d9066efb80d181b4eec290
Steven Hahn <hahnse@ornl.gov>
2016-07-07 16:47:52 -0400

    add mpi support.

--- h5py.rb
+++ h5py.rb
@@ -2,10 +2,15 @@ class H5py < Formula
   desc "Pythonic interface to the HDF5 binary data format"
   homepage "http://www.h5py.org"
   url "https://github.com/h5py/h5py/archive/2.6.0.tar.gz"
-  version "2.6.0"
   sha256 "7fec1d6f19418b3a624bbb90c7baa105952c0bb6cfbb6676f436fac44fc6ccfb"
 
   depends_on "hdf5"
+  depends_on :mpi => :optional
+  depends_on "mpi4py" if build_with? "mpi"
+
+  option "without-python", "Build without python2 support"
+  depends_on :python => :recommended if MacOS.version <= :snow_leopard
+  depends_on :python3 => :optional
 
   resource "cython" do
     url "https://pypi.python.org/packages/b1/51/bd5ef7dff3ae02a2c6047aa18d3d06df2fb8a40b00e938e7ea2f75544cac/Cython-0.24.tar.gz"
@@ -13,16 +18,23 @@ class H5py < Formula
   end
 
   def install
-    ENV.prepend_create_path "PYTHONPATH", buildpath+"lib/python2.7/site-packages"
-    resource("cython").stage do
-      system "python", "setup.py", "build", "install", "--prefix=#{buildpath}"
-    end
+    Language::Python.each_python(build) do |python, version|
+      ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python#{version}/site-packages"
+      resource("cython").stage do
+        system python, *Language::Python.setup_install_args(libexec/"vendor")
+      end
 
-    system "python", "setup.py", "configure", "--hdf5=#{Formula["homebrew/science/hdf5"].opt_prefix}", "build"
-    system "python", *Language::Python.setup_install_args(prefix)
+      args = ["configure","--hdf5=#{Formula["homebrew/science/hdf5"].opt_prefix}"]
+      args << "--mpi" if build.with? "mpi"
+
+      ENV.prepend_create_path "PYTHONPATH", lib/"python#{version}/site-packages"
+      system python, *Language::Python.setup_install_args(prefix), *args
+    end
   end
 
   test do
-    system "python -c \"import h5py; print(h5py.__version__)\""
+    Language::Python.each_python(build) do |python, _|
+      system python, "-c", "\"import h5py; print(h5py.__version__)\""
+    end
   end
 end
