0855d741b4310d73babbe1e6fc637c62b1c871cd
Steven Hahn <hahnse@ornl.gov>
2016-07-07 18:44:37 -0400

    fix python3 dependency.

--- h5py.rb
+++ h5py.rb
@@ -4,13 +4,20 @@ class H5py < Formula
   url "https://github.com/h5py/h5py/archive/2.6.0.tar.gz"
   sha256 "7fec1d6f19418b3a624bbb90c7baa105952c0bb6cfbb6676f436fac44fc6ccfb"
 
-  depends_on "hdf5"
-  depends_on :mpi => :optional
-  depends_on "mpi4py" if build_with? "mpi"
-
   option "without-python", "Build without python2 support"
   depends_on :python => :recommended if MacOS.version <= :snow_leopard
   depends_on :python3 => :optional
+
+  depends_on :mpi => :optional
+  depends_on "homebrew/science/hdf5" => ["with-mpi"] if build.with? :mpi
+
+  if build.with? :mpi
+    depends_on "mpi4py" => ["with-python3"] if build.with? :python3
+  end
+
+  if build.with? :python3
+    depends_on "homebrew/python/numpy" => ["with-python3"]
+  end
 
   resource "cython" do
     url "https://pypi.python.org/packages/b1/51/bd5ef7dff3ae02a2c6047aa18d3d06df2fb8a40b00e938e7ea2f75544cac/Cython-0.24.tar.gz"
@@ -24,8 +31,8 @@ class H5py < Formula
         system python, *Language::Python.setup_install_args(libexec/"vendor")
       end
 
-      args = ["configure","--hdf5=#{Formula["homebrew/science/hdf5"].opt_prefix}"]
-      args << "--mpi" if build.with? "mpi"
+      args = ["configure", "--hdf5=#{Formula["homebrew/science/hdf5"].opt_prefix}"]
+      args << "--mpi" if build.with? :mpi
 
       ENV.prepend_create_path "PYTHONPATH", lib/"python#{version}/site-packages"
       system python, *Language::Python.setup_install_args(prefix), *args
