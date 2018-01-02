f71078b282b8176eba5e04d103d4bd7b56620387
Steven Hahn <hahnse@ornl.gov>
2018-01-02 10:23:19 -0500

    add formulas missing after homebrew-science deprecation.

---
+++ nexusformat.rb
@@ -0,0 +1,38 @@
+class Nexusformat < Formula
+  desc "Common data format for neutron, x-ray, and muon science"
+  homepage "http://www.nexusformat.org"
+  url "https://github.com/nexusformat/code/archive/v4.4.3.tar.gz"
+  sha256 "e78a116feb2ebd04de31a8d8707c65e8e15a64aa8999a40fea305e3909bd6533"
+  revision 6
+
+  option :cxx11
+
+  deprecated_option "hdf@4" => "hdf4"
+
+  depends_on "cmake" => :build
+  depends_on "libmxml"
+  cxx11dep = build.cxx11? ? ["c++11"] : []
+  depends_on "hdf5" => cxx11dep
+  depends_on "readline" => :recommended
+  depends_on "hdf4" => :recommended
+  depends_on "doxygen" => :optional
+
+  def install
+    ENV.cxx11 if build.cxx11?
+    cmake_args = std_cmake_args
+    cmake_args << "-DENABLE_APPS=TRUE"
+    cmake_args << "-DENABLE_CXX=TRUE"
+    cmake_args << "-DENABLE_MXML=TRUE"
+    cmake_args << "-DENABLE_HDF4=TRUE" if build.with? "hdf4"
+    system "cmake", ".", *cmake_args
+    system "make"
+    # enable once test failures are resolved upstream
+    # https://github.com/nexusformat/code/issues/443
+    # system "make", "test"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/nxdir"
+  end
+end
