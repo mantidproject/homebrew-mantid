d1c91930624734717d1408fc70adf6598627c0a9
Steven Hahn <hahnse@ornl.gov>
2018-02-27 13:46:37 -0500

    add clazy.

---
+++ clazy.rb
@@ -0,0 +1,26 @@
+class Clazy < Formula
+  desc "Qt oriented code checker based on clang framework"
+  homepage "https://github.com/KDE/clazy"
+  url "https://download.kde.org/stable/clazy/1.2/src/clazy-1.2.tar.xz"
+  sha256 "c68fed3d719c23ffefeedd88a02984bf37dba6a55dad6359786909a9e5778f91"
+  revision 1
+
+  head "git://anongit.kde.org/clazy.git"
+
+  depends_on "cmake" => :build
+
+  depends_on "llvm"
+  depends_on "qt"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_TESTING=OFF"
+    args << "-DCMAKE_INSTALL_MANDIR=#{man}"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      prefix.install "install_manifest.txt"
+    end
+  end
+end
