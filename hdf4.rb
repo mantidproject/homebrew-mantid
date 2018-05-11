76ef79625e013101a0b57fc39753a904f3737e2e
Steven Hahn <hahnse@ornl.gov>
2018-05-11 09:06:31 -0400

    hdf4

--- hdf4.rb
+++ hdf4.rb
@@ -43,12 +43,6 @@ class Hdf4 < Formula
     end
   end
 
-  def caveats; <<-EOS.undent
-      HDF4 has been superseeded by HDF5.  However, the API changed
-      substantially and some programs still require the HDF4 libraries in order
-      to function.
-    EOS
-  end
 end
 
 __END__
