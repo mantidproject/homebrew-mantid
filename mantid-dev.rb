f16acb94069c7384beb9131907abfeecede613ca
Gagik Vardanyan <vardanyan@ill.fr>
2018-06-14 17:30:51 +0200

    added specific flags for the dependencies

--- mantid-dev.rb
+++ mantid-dev.rb
@@ -10,7 +10,7 @@ class MantidDev < Formula
   version '1.1'
   sha256 "e9f83c057e70082c48f5576d3e7d0d257f55f1835ce659239c64445bb01da551"
   
-  depends_on "boost-python"
+  depends_on "boost-python" => '--c++11'
   depends_on "boost-mpi"
   depends_on "ccache"
   depends_on "clang-format"
@@ -20,22 +20,23 @@ class MantidDev < Formula
   depends_on "google-perftools"
   depends_on "graphviz"
   depends_on "gsl"
-  depends_on "hdf5"
-  depends_on "h5py"
+  depends_on "hdf5" => '--c++11'
+  depends_on "h5py" => '--without-python'
   depends_on "jsoncpp"
   depends_on "libmxml"
   depends_on "librdkafka"
   depends_on "muparser"
-  depends_on "nexusformat"
+  depends_on "nexusformat" => '--c++11'
+  depends_on "ninja" => '--without-test'
   depends_on "opencascade" 
   depends_on "poco"
   depends_on "qt@4"
-  depends_on "qscintilla2qt4"
+  depends_on "qscintilla2qt4" => '--without-python'
   depends_on "qwt5"
   depends_on "qwtplot3d"
   depends_on "pyqt@4"
-  depends_on "sip"
-  depends_on "tbb"
+  depends_on "sip" => '--without-python'
+  depends_on "tbb" => '--c++11'
 
   def install
       
