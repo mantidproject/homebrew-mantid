0c6170d60bcf8470646535f523ccd5a241738d40
Gagik Vardanyan <vardanyan@ill.fr>
2018-10-26 10:04:50 -0400

    full path for brew

--- mantid-dev.rb
+++ mantid-dev.rb
@@ -53,8 +53,8 @@ class MantidDev < Formula
 	
   def install
     #prefix.install("pyport.patch")
-    system "brew uninstall --ignore-dependencies python@2"
-    system "brew uninstall --ignore-dependencies python"
+    system "/usr/local/bin/brew uninstall --ignore-dependencies python@2"
+    system "/usr/local/bin/brew uninstall --ignore-dependencies python"
     virtualenv_install_with_resources
   end
 	
