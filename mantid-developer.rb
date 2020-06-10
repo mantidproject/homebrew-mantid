db35526897d5ea38e26c936c6524dc77c22e1e42
Martyn Gigg <martyn.gigg@stfc.ac.uk>
2020-06-10 09:46:06 +0100

    Use boost-python3 to depend on python 3.8

--- mantid-developer.rb
+++ mantid-developer.rb
@@ -4,7 +4,7 @@ class MantidDeveloper < Formula
   # this is really a metapackage only
   url 'https://raw.githubusercontent.com/mantidproject/mantid/master/buildconfig/pyport.patch' #:using => :curl
   homepage "http://developer.mantidproject.org/BuildingOnOSX.html"
-  version '1.5'
+  version '1.6'
   sha256 "e9f83c057e70082c48f5576d3e7d0d257f55f1835ce659239c64445bb01da551"
 
   # framework dependencies
@@ -14,7 +14,7 @@ class MantidDeveloper < Formula
   depends_on "pkg-config"
   depends_on "poco"
   depends_on "boost"
-  depends_on "boost-python"
+  depends_on "boost-python3"
   depends_on "gsl"
   depends_on "hdf5" => "build-from-source"
   depends_on "libomp"
